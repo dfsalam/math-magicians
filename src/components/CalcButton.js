04c9dd7b5e499c47b0b7249745b03b41f68df1e6
dfsalam <dfsalam@gmail.com>
2023-02-17 00:15:50 -0500

    Events - Add onclick function. - Add hook state inside Calculator.js. - Add display function.

--- src/components/CalcButton.js
+++ src/components/CalcButton.js
@@ -3,16 +3,19 @@ import PropTypes from 'prop-types';
 export default function CalcButton(props) {
   const { content } = props;
   const { btnStyle } = props;
+  const { clickFunction } = props;
   return (
-    <button type="button" className={btnStyle}>{content}</button>
+    <button type="button" className={btnStyle} onClick={clickFunction}>{content}</button>
   );
 }
 
 CalcButton.propTypes = {
   content: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
   btnStyle: PropTypes.string,
+  clickFunction: PropTypes.func,
 };
 
 CalcButton.defaultProps = {
   btnStyle: 'defBtn',
+  clickFunction: () => {},
 };
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,32 +1,57 @@
+import React, { useState } from 'react';
 import './calc.css';
 import CalcButton from './CalcButton';
 import CalcDisplay from './CalcDisplay';
-import '../logic/calculate';
-import '../logic/operate';
+import calculate from '../logic/calculate';
 
 export default function Calculator() {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+  const clickFunction2 = (e) => {
+    setState((prevState) => calculate(prevState, e.target.innerText));
+  };
+  const { total } = state;
+  const { next } = state;
+  const { operation } = state;
+  const display = () => {
+    if (operation && next) {
+      return next;
+    }
+    if (operation) {
+      return operation;
+    }
+    if (next) {
+      return next;
+    }
+    return total;
+  };
+  console.log(state);
+  const show = display();
   return (
     <div className="calc__container">
-      <CalcDisplay disStyle="disStyle" />
-      <CalcButton content="AC" />
-      <CalcButton content="+/-" />
-      <CalcButton content="%" />
-      <CalcButton content="÷" btnStyle="orange" />
-      <CalcButton content={7} />
-      <CalcButton content={8} />
-      <CalcButton content={9} />
-      <CalcButton content="x" btnStyle="orange" />
-      <CalcButton content={4} />
-      <CalcButton content={5} />
-      <CalcButton content={6} />
-      <CalcButton content="-" btnStyle="orange" />
-      <CalcButton content={3} />
-      <CalcButton content={2} />
-      <CalcButton content={1} />
-      <CalcButton content="+" btnStyle="orange" />
-      <CalcButton content={0} btnStyle="zero" />
-      <CalcButton content="." />
-      <CalcButton content="=" btnStyle="orange" />
+      <CalcDisplay disStyle="disStyle" pHolder={show} />
+      <CalcButton content="AC" clickFunction={clickFunction2} />
+      <CalcButton content="+/-" clickFunction={clickFunction2} />
+      <CalcButton content="%" clickFunction={clickFunction2} />
+      <CalcButton content="÷" btnStyle="orange" clickFunction={clickFunction2} />
+      <CalcButton content="7" clickFunction={clickFunction2} />
+      <CalcButton content="8" clickFunction={clickFunction2} />
+      <CalcButton content="9" clickFunction={clickFunction2} />
+      <CalcButton content="x" btnStyle="orange" clickFunction={clickFunction2} />
+      <CalcButton content="4" clickFunction={clickFunction2} />
+      <CalcButton content="5" clickFunction={clickFunction2} />
+      <CalcButton content="6" clickFunction={clickFunction2} />
+      <CalcButton content="-" btnStyle="orange" clickFunction={clickFunction2} />
+      <CalcButton content="3" clickFunction={clickFunction2} />
+      <CalcButton content="2" clickFunction={clickFunction2} />
+      <CalcButton content="1" clickFunction={clickFunction2} />
+      <CalcButton content="+" btnStyle="orange" clickFunction={clickFunction2} />
+      <CalcButton content="0" btnStyle="zero" clickFunction={clickFunction2} />
+      <CalcButton content="." clickFunction={clickFunction2} />
+      <CalcButton content="=" btnStyle="orange" clickFunction={clickFunction2} />
     </div>
   );
 }
